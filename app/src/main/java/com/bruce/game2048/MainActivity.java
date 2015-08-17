8caf0cceb45a099b2bd546dba70e89e1068983ff
Bruce <dinghanyu221@126.com>
2015-08-17 09:32:10 +0800

    Initial commit

---
+++ app/src/main/java/com/bruce/game2048/MainActivity.java
@@ -0,0 +1,54 @@
+package com.bruce.game2048;
+
+/**
+ * Created by Bruce on 2015/8/16.
+ */
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    public MainActivity() {
+        mainActivity = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvScore = (TextView) findViewById(R.id.tvScore);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void clearScore() {
+        score = 0;
+        showScore();
+    }
+
+    public void showScore() {
+        tvScore.setText(score + "");
+    }
+
+    public void addScore(int s) {
+        score += s;
+        showScore();
+    }
+
+    private int score = 0;
+    private TextView tvScore;
+    private static MainActivity mainActivity = null;
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+}
